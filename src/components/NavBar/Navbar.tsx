3eb33c035d3ce5cee681c36bc5e21f45e5355cdc
Jeremy Smart <jeremy3141592@gmail.com>
2025-02-05 02:11:22 -0500

    add kevlar

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -65,6 +65,16 @@ const NavBar = () => {
                                 Favorites
                             </NavLink>
                         </NavItem>
+
+                        <NavItem>
+                            <NavLink href="/kevlar">
+                                <FontAwesomeIcon
+                                    icon="fa-solid fa-shield"
+                                    className="mr-1"
+                                />
+                                Toggle Kevlar (max once per day)
+                            </NavLink>
+                        </NavItem>
                         {isEboardOrRTP(oidcUser) && (
                             <>
                                 <NavItem>
