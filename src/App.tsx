7b3397b0e4b87bb2d02367433c4a19abbe3c080e
Jeremy Smart <jeremy3141592@gmail.com>
2025-02-05 20:52:16 -0500

    add kevlar route

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
                             path="/favorites"
                             element={<Storage storageType="FAVORITES" />}
                         />
+                        <Route path="/kevlar" element={<Home />} />
                         <Route path="/report" element={<SubmitReport />} />
                         <Route path="/reports" element={<Reports />} />
                     </Routes>
